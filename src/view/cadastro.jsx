e7323a275d2b4231ecf6244f4ae8e4fea3c428fb
HebertPereira <hebertpktd@gmail.com>
2020-06-15 16:43:33 -0300

    update 4

--- src/view/cadastro.jsx
+++ src/view/cadastro.jsx
@@ -7,7 +7,8 @@ const estadoInicial = {
     sku: '',
     descricao: '',
     preco: 0,
-    fornecedor: ''
+    fornecedor: '',
+    acesso: true
 }
 
 class CadastroProduto extends React.Component {
@@ -27,87 +28,89 @@ class CadastroProduto extends React.Component {
     }
 
     validaAcesso = () => {
-        //let location = useLocation();
-        
 
-        let recuperarDB = localStorage.getItem('_LOGIN-DB')
+        let puxarDB = localStorage.getItem('_LOGIN-DB')
 
-        let a = JSON.parse(recuperarDB)
+        let recuperarDB = JSON.parse(puxarDB)
 
-        console.log(a)
-        console.log(a.nome, a.senha)
-        //const nomeIdentificator = recuperarDB[]
-        //console.log(nomeIdentificator)
+        console.log(recuperarDB)
+        console.log(recuperarDB.nome, recuperarDB.senha)
 
-        let localAtual = '#/'+ a.nome.strip() +'/Cadastro'
-        //let enderecoDestino = location.pathname;
+        let localAtual = '#/' + recuperarDB.nome.replace(' ', '') + '/Cadastro'
+        let enderecoDestino = window.location.href;
 
         console.log(localAtual)
-        //console.log(enderecoDestino)
-        
-
-        //if (localAtual === enderecoDestino){
-          //  console.log('funciona')
-        //}
+        console.log(enderecoDestino)
+        //Preciso terminar...
+        this.setState({acesso: true})
     }
 
     render() {
-        return (
-
-            <div className="card">
-                <form id="frmProduto">
-                    <div className="card-header">
-                        Cadastre seu produto
+        //this.validaAcesso()
+        if (this.state.acesso === true) {
+            return (
+                <div className="card">
+                    <form id="frmProduto">
+                        <div className="card-header">
+                            Cadastre seu produto
                 </div>
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label>Nome: *</label>
-                                    <input type="text" name="nome" value={this.setState.nome} onChange={this.OnChange} className="form-control" placeholder="Digite o nome ..." />
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Nome: *</label>
+                                        <input type="text" name="nome" value={this.setState.nome} onChange={this.OnChange} className="form-control" placeholder="Digite o nome ..." />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>SKU: *</label>
+                                        <input type="text" name="sku" value={this.setState.sku} onChange={this.OnChange} className="form-control" placeholder="Digite o Sku ..." />
+                                    </div>
                                 </div>
                             </div>
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label>SKU: *</label>
-                                    <input type="text" name="sku" value={this.setState.sku} onChange={this.OnChange} className="form-control" placeholder="Digite o Sku ..." />
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="form-group">
+                                        <label>Descrição</label>
+                                        <textarea className="form-control" value={this.setState.descricao} onChange={this.OnChange} placeholder="Digite a descrição ..."></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Preço: </label>
+                                        <input type="text" name="preco" value={this.setState.preco} onChange={this.OnChange} className="form-control" placeholder="Digite o Preço ..." />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Fornecedor: </label>
+                                        <input type="text" name="fornecedor" value={this.setState.fornecedor} onChange={this.OnChange} className="form-control" placeholder="Digite o Fornecedor ..." />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-1">
+                                    <button className="btn btn-success" onClick={this.validaAcesso}>Adicionar</button>
+                                </div>
+                                <div className="col-md-1">
+                                    <button className="btn btn-warning" onClick={this.limpaCampos}>Limpar</button>
                                 </div>
                             </div>
                         </div>
-                        <div className="row">
-                            <div className="col-md-12">
-                                <div className="form-group">
-                                    <label>Descrição</label>
-                                    <textarea className="form-control" value={this.setState.descricao} onChange={this.OnChange} placeholder="Digite a descrição ..."></textarea>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label>Preço: </label>
-                                    <input type="text" name="preco" value={this.setState.preco} onChange={this.OnChange} className="form-control" placeholder="Digite o Preço ..." />
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label>Fornecedor: </label>
-                                    <input type="text" name="fornecedor" value={this.setState.fornecedor} onChange={this.OnChange} className="form-control" placeholder="Digite o Fornecedor ..." />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-md-1">
-                                <button className="btn btn-success" onClick={this.validaAcesso}>Adicionar</button>
-                            </div>
-                            <div className="col-md-1">
-                                <button className="btn btn-warning" onClick={this.limpaCampos}>Limpar</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        )
+                    </form>
+                </div>
+            )
+        }
+        else{
+            return(
+                <div className="card">
+                    <label>Não tem acesso</label>
+                </div>
+            )
+        }
     }
 }
 
--- src/view/login.jsx
+++ src/view/login.jsx
@@ -1,49 +1,62 @@
 import React from 'react';
 import '../css/login.css';
 import { withRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
 
-function Login() {
-
-    const LOGIN = '_LOGIN-DB'
-    let nomeUser = '';
-    let senhaUser = '';
-
-    return (
-        <>
-            <div className="card-mod">
-                <div className="card-header">Login</div>
-                <div className="card-body">
-                    <div className="col-md-12">
-                        <div className="form-group">
-                            <label>Nome/E-mail:</label>
-                            <input type="text" name="nome/email" className="form-control" placeholder="Digite o nome ou e-mail ..." onChange={(event) => {
-                                nomeUser = event.target.value
-                            }} />
-                        </div>
-                    </div>
-                    <div className="col-md-12">
-                        <div className="form-group">
-                            <label>Senha:</label>
-                            <input type="password" name="senha" className="form-control" placeholder="Digite a sua senha..." onChange={(event) => {
-                                senhaUser = event.target.value
-                            }} />
-                        </div>
-                    </div>
-                    <button className="btn btn-primary" onClick={() => {
-                        console.log(nomeUser)
-                        if (nomeUser !== '' || senhaUser !== '') {
-                            nomeUser = nomeUser.strip()
-                            const login = {nome: nomeUser, senha: senhaUser}
-                            localStorage.setItem(LOGIN, JSON.stringify(login))
-                            alert('Login efetuado com sucesso')
-                        } else {
-                            alert('Dados invalidos! Verifique os campos e tente novamente.')
-                        }
-                    }}>Vamos la!</button>
-                </div>
-            </div>
-        </>
-    )
+const LOGIN = '_LOGIN-DB'
+const valorPadrao = {
+    nomeUser: '',
+    senhaUser: ''
 }
 
-export default withRouter (Login);
+
+
+class Login extends React.Component {
+
+    state = valorPadrao;
+    render() {
+        return (
+            <>
+                <div className="card-mod">
+                    <div className="card-header">Login</div>
+                    <div className="card-body">
+                        <form>
+                            <div className="col-md-12">
+                                <div className="form-group">
+                                    <label>Nome/E-mail:</label>
+                                    <input type="text" name="nomeEmail" className="form-control" placeholder="Digite o nome ou e-mail ..." onChange={(event) => {
+                                        this.setState({ nomeUser: event.target.value })
+                                        console.log(this.state.nomeUser)
+                                    }} />
+                                </div>
+                            </div>
+                        </form>
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <label>Senha:</label>
+                                <input type="password" name="senha" className="form-control" placeholder="Digite a sua senha..." onChange={(event) => {
+                                    this.setState({ senhaUser: event.target.value })
+                                    console.log(this.state.senhaUser)
+                                }} />
+                            </div>
+                        </div>
+                        <button className="btn btn-primary" onClick={() => {
+                            console.log(this.state.nomeUser)
+                            if (this.state.nomeUser !== '' || this.state.senhaUser !== '') {
+                                //this.setState({nomeUser : this.state.nomeUser.strip()})
+                                const login = { nome: this.state.nomeUser, senha: this.state.senhaUser }
+                                localStorage.setItem(LOGIN, JSON.stringify(login))
+                                alert('Login efetuado com sucesso')
+                                window.location.href = '#/' + this.state.nomeUser.replace(' ', '') +'/Cadastro'
+                            } else {
+                                alert('Dados invalidos! Verifique os campos e tente novamente.')
+                            }
+                        }}>Vamos la!</button>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default withRouter(Login);
